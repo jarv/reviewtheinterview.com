781b9a33850ae65290abf3addcd89f8c1fbae090
John Jarvis <john@jarv.org>
2016-11-23 19:45:57 +0100

    add cron

--- Makefile
+++ Makefile
@@ -16,8 +16,8 @@ runserver:
 flower:
 	cd watcher;DJANGO_SETTINGS_MODULE=watcher.settings celery -A watcher.tasks flower --port=5556
 sync:
-	aws --region us-east-1 --profile reviewtheinterview s3 sync --exclude "pending-reviews/pending-reviews.json" --exclude "reviews/reviews.json" --exclude "js/review-the-interview.js" --exclude "css/review-the-interview.css" --exclude "css/review-the-interview.css.map" --exclude "index.html" --acl public-read $(OUTPUTDIR)/ s3://testing.reviewtheinterview.com
+	aws --region us-east-1 --profile reviewtheinterview s3 sync --exclude "pending-reviews/pending-reviews.json" --exclude "reviews/reviews.json" --exclude "js/review-the-interview.js" --exclude "css/review-the-interview.css" --exclude "css/review-the-interview.css.map" --acl public-read $(OUTPUTDIR)/ s3://testing.reviewtheinterview.com
 	aws --region us-east-1 --profile reviewtheinterview s3 cp --content-type "application/javascript" --cache-control "no-cache, no-store, must-revalidate" --expires 0 --acl public-read static/js/review-the-interview.js s3://testing.reviewtheinterview.com/js/review-the-interview.js
 	aws --region us-east-1 --profile reviewtheinterview s3 cp --content-type "text/css" --cache-control "no-cache, no-store, must-revalidate" --expires 0 --acl public-read static/css/review-the-interview.css s3://testing.reviewtheinterview.com/css/review-the-interview.css
 	aws --region us-east-1 --profile reviewtheinterview s3 cp --content-type "application/octet-stream" --cache-control "no-cache, no-store, must-revalidate" --expires 0 --acl public-read static/css/review-the-interview.css.map s3://testing.reviewtheinterview.com/css/review-the-interview.css.map
-	aws --region us-east-1 --profile reviewtheinterview s3 cp --content-type "application/octet-stream" --cache-control "no-cache, no-store, must-revalidate" --expires 0 --acl public-read static/index.html s3://testing.reviewtheinterview.com/index.html
+#	aws --region us-east-1 --profile reviewtheinterview s3 cp --content-type "application/octet-stream" --cache-control "no-cache, no-store, must-revalidate" --expires 0 --acl public-read static/index.html s3://testing.reviewtheinterview.com/index.html
--- lambda/cron/_src/reviewtheinterview.py
+++ lambda/cron/_src/reviewtheinterview.py
@@ -1,10 +1,67 @@
 import logging
 import time
+import boto3
+from boto3.dynamodb.conditions import Key
+from cStringIO import StringIO
+import json
+import decimal
 
 LOG = logging.getLogger()
-LOG.setLevel(logging.DEBUG)
+LOG.setLevel(logging.ERROR)
 
+BUCKET = "testing.reviewtheinterview.com"
+PENDING_KEY = "pending-reviews/pending-reviews.json"
+REVIEW_KEY = "reviews/reviews.json"
+SUBMISSIONS_TABLE_NAME = 'reviewtheinterview-submissions'
+FIELDS = ["id", "emoji", "company", "create_time", "position", "review"]
+
+PENDING = Key('approve').lt(2)
+APPROVED = Key('approve').gte(2)
+REJECTED = Key('reject').gte(2)
+
+
+def value_convert(v):
+    if isinstance(v, decimal.Decimal):
+        if v % 1 > 0:
+            return float(v)
+        else:
+            return int(v)
+    return v
+
+
+def delete_item(key_id):
+    dynamo_submissions = boto3.resource('dynamodb').Table(SUBMISSIONS_TABLE_NAME)
+    dynamo_submissions.delete_item(Key={'id': key_id})
+
+def get_items(p):
+    return sorted([{k: value_convert(d[k]) for k in FIELDS} for d in p['Items']], key=lambda d: d['create_time'], reverse=True)
 
 def handler(event, context):
-    LOG.debug(event)
+    s = boto3.resource('dynamodb').Table(SUBMISSIONS_TABLE_NAME)
+    s3 = boto3.resource('s3')
+
+    # rejected
+    r = s.scan(FilterExpression=REJECTED)
+    if 'Items' in r:
+        for i in r['Items']:
+            LOG.info("Deleting {}".format(i['id']))
+            print("Deleting {}".format(i['id']))
+            delete_item(i['id'])
+
+    # pending
+    p = s.scan(FilterExpression=PENDING)
+    if 'Items' in p:
+        pending = get_items(p)
+        pending_contents = StringIO(json.dumps(pending, indent=4, sort_keys=True))
+        bucket = s3.Bucket(BUCKET)
+        bucket.put_object(ContentType='application/json', CacheControl='no-cache, no-store, must-revalidate', Expires=0, ACL='public-read', Bucket=BUCKET, Key=PENDING_KEY, Body=pending_contents.read())
+
+    # approved
+    a = s.scan(FilterExpression=APPROVED)
+    if 'Items' in a:
+        approved = get_items(a)
+        approved_contents = StringIO(json.dumps(approved, indent=4, sort_keys=True))
+        bucket = s3.Bucket(BUCKET)
+        bucket.put_object(ContentType='application/json', CacheControl='no-cache, no-store, must-revalidate', Expires=0, ACL='public-read', Bucket=BUCKET, Key=REVIEW_KEY, Body=approved_contents.read())
+
     return {'status': 'success', 'time': time.time()}
